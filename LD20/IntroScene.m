707d7cb288fd71a29600331f18b862999cdfa9b2
tomek <tomek@tomekwyszomirskis-MacBook-Pro.local>
2011-05-02 00:43:40 +0200

    added title screen view and intro screen views move anims

--- LD20/IntroScene.m
+++ LD20/IntroScene.m
@@ -14,6 +14,7 @@
 
 CGPoint screenCenter;
 
+CCSprite* intro0;
 CCSprite* intro1;
 CCSprite* intro2;
 CCSprite* laserCannon;
@@ -21,8 +22,12 @@ CCSprite* laserCannon;
 int currentScreenNr = 0;
 BOOL duringFadeOut = NO;
 
-const float FADE_IN_ANIM_LEN = 1.4;
-const float FADE_OUT_ANIM_LEN = 1.4;
+const float FADE_IN_ANIM_LEN = 0.8;
+const float FADE_OUT_ANIM_LEN = 0.8;
+
+const float MOVE_ANIM_LEN = 1.05;
+int animOffset = 20; // intro animation offsets
+
 
 +(CCScene*) scene {
     CCScene* scene = [CCScene node];
@@ -39,12 +44,12 @@ const float FADE_OUT_ANIM_LEN = 1.4;
         CGSize size = [[CCDirector sharedDirector] winSize];
         screenCenter = CGPointMake(size.width/2, size.height/2);
 
+        intro0 = [[CCSprite spriteWithFile:@"titleScreen.png"] retain];
         intro1 = [[CCSprite spriteWithFile:@"Intro1.png"] retain];
         intro2 = [[CCSprite spriteWithFile:@"Intro2.png"] retain];
         laserCannon = [[CCSprite spriteWithFile:@"LaserCannonScreen.png"] retain];
         
-        [self showIntroViewWithSprite:intro1];
-        currentScreenNr = 1;
+        [self showIntroViewWithSprite:intro0];
         
         self.isTouchEnabled = YES;
     }
@@ -52,6 +57,7 @@ const float FADE_OUT_ANIM_LEN = 1.4;
 }
 
 - (void)dealloc {
+    [intro0 release];
     [intro1 release];
     [intro2 release];
     [laserCannon release];
@@ -73,29 +79,43 @@ const float FADE_OUT_ANIM_LEN = 1.4;
 - (void)dismissViewWithFadeOut {
     id fadeAnim = [CCFadeOut actionWithDuration:FADE_OUT_ANIM_LEN];
     
-    if (currentScreenNr == 1) {
+    if (currentScreenNr == 0) {
+        [intro0 stopAllActions];
+        [intro0 runAction:fadeAnim];
+    } else if (currentScreenNr == 1) {
         [intro1 stopAllActions];
         [intro1 runAction:fadeAnim];
-//        [intro1 runAction:[CCMoveBy actionWithDuration:2 position:CGPointMake(10, 10)]];
+        [intro1 runAction:[CCMoveBy actionWithDuration:MOVE_ANIM_LEN position:CGPointMake(animOffset, animOffset)]];
     } else if (currentScreenNr == 2) {
         [intro2 stopAllActions];
         [intro2 runAction:fadeAnim];
+        [intro2 runAction:[CCMoveBy actionWithDuration:MOVE_ANIM_LEN position:CGPointMake(animOffset, -animOffset)]];
     } else if (currentScreenNr == 3) {
         [laserCannon stopAllActions];
         [laserCannon runAction:fadeAnim];
+        [laserCannon runAction:[CCMoveBy actionWithDuration:MOVE_ANIM_LEN position:CGPointMake(-animOffset, -animOffset)]];
     }
 }
 
-- (void)showView2:(ccTime)dt {
-//- (void)showView2 {    
+- (void)showView1:(ccTime)dt { 
+    [self unschedule:@selector(showView1:)];
+    [self showIntroViewWithSprite:intro1];
+    [intro1 runAction:[CCMoveBy actionWithDuration:MOVE_ANIM_LEN position:CGPointMake(animOffset, animOffset)]];
+    currentScreenNr = 1;
+}
+
+- (void)showView2:(ccTime)dt { 
     [self unschedule:@selector(showView2:)];
     [self showIntroViewWithSprite:intro2];
+    [intro2 runAction:[CCMoveBy actionWithDuration:MOVE_ANIM_LEN position:CGPointMake(animOffset, -animOffset)]];
     currentScreenNr = 2;
 }
 
 - (void)showView3:(ccTime)dt {
-    [self unschedule:@selector(showView3:)];    
+    [self unschedule:@selector(showView3:)];   
+    laserCannon.position = CGPointMake(laserCannon.position.x - animOffset, laserCannon.position.y - animOffset);
     [self showIntroViewWithSprite:laserCannon];
+    [laserCannon runAction:[CCMoveBy actionWithDuration:MOVE_ANIM_LEN position:CGPointMake(-animOffset, -animOffset)]];
     currentScreenNr = 3;
 }
 
@@ -116,7 +136,11 @@ const float FADE_OUT_ANIM_LEN = 1.4;
 }
 
 - (void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event {
-    if (currentScreenNr == 1) {
+    if (currentScreenNr == 0) {
+        [self dismissViewWithFadeOut];
+        [self schedule:@selector(showView1:) interval:FADE_OUT_ANIM_LEN];
+        //CCTimer* timer = [[CCTimer alloc] initWithTarget:self selector:@selector(showView1:) interval:FADE_OUT_ANIM_LEN];
+    } else if (currentScreenNr == 1) {
         [self dismissViewWithFadeOut];
         [self schedule:@selector(showView2:) interval:FADE_OUT_ANIM_LEN];
         //CCTimer* timer = [[CCTimer alloc] initWithTarget:self selector:@selector(showView2:) interval:FADE_OUT_ANIM_LEN];
